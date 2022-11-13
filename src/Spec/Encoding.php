2b1d9f6c8f7bb1d9a522ed5ae87d83e0de681bf2
Marek Bartoš <bartos.developer152@gmail.com>
2022-11-14 00:22:18 +0100

    wip

--- src/Spec/Encoding.php
+++ src/Spec/Encoding.php
@@ -24,13 +24,20 @@ final class Encoding implements SpecObject
 	public function toArray(): array
 	{
 		//TODO - https://spec.openapis.org/oas/v3.1.0#encoding-object
+		//TODO - pro style, explode a allowReserved specifikace uvádí změnu chování v případě, že jsou definované explicitně
+		//		- implicitní default se tedy liší od explicitního
 		$data = [];
 
+		//TODO - mime type
+		//		- získávání defaults
+		//		- více mime types - čárkou oddělený seznam
+		//		- application/* apod. - ověřit i ostatní místa užití
 		if ($this->contentType !== null) {
 			$data['contentType'] = $this->contentType;
 		}
 
 		//TODO - totéž co v Response?
+		//		- nedovoluje Content-Type
 		if ($this->headers !== []) {
 			$data['headers'] = SpecUtils::specsToArray($this->headers);
 		}
@@ -39,6 +46,7 @@ final class Encoding implements SpecObject
 			$data['style'] = $this->style->value;
 		}
 
+		// TODO - pro style=form je true, jinak false
 		if ($this->explode) {
 			$data['explode'] = $this->explode;
 		}
